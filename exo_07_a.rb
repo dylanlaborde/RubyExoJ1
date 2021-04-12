4406cabfb4acce8a5b33d16d40f100ef464d1561
dylanlaborde <dylan4047@gmail.com>
2021-04-12 14:11:08 +0200

    resolution des exos

---
+++ exo_07_a.rb
@@ -0,0 +1,4 @@
+puts "Bonjour, c'est quoi ton blase ?"
+user_name = gets.chomp
+#gets.champ permet de recurerer la saisi de l'utilisateur 
+puts user_names
---
+++ exo_07_c.rb
@@ -0,0 +1,10 @@
+user_name = gets.chomp
+puts user_name
+
+#la diff entre les 3 programme 07 est juste la maniere dont elle est restituée a l'utilisateur 
+
+#dans la version b on a une appoche plus tournée vers l'utilisateur avec un question , le oment de saisir est plus clair avec un > qui permet d'etre plus intuitif pour l'utilisateur 
+
+# version c aucune instruction a l'utilisateur pas intuitif 
+
+# version a un melange des deux 
