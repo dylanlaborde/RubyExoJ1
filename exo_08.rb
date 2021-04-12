c995fe6cb9629efedd89104a55971f3817321d5d
dylanlaborde <dylan4047@gmail.com>
2021-04-12 14:17:09 +0200

    salution + reponse + User_name

---
+++ exo_08.rb
@@ -0,0 +1,6 @@
+#Écris un programme exo_08.rb qui demande le prénom de l'utilisateur, et qui salue l'utilisateur avec "Bonjour, prénom !"
+
+puts "salut c'est quoi ton ptit nom"
+user_name = gets.chomp
+
+puts "bonjour, "+user_name+" !"
