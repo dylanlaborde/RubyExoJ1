20a3b7ddc51a5fa663b016e3e646872e532911e0
dylanlaborde <dylan4047@gmail.com>
2021-04-12 16:12:31 +0200

    ajout de fonction pour l"optimisation et pouvoir rentrer un nouvel valeur si pas entre 1 et 50

--- exo_12.rb
+++ exo_12.rb
@@ -4,13 +4,23 @@ puts "ecrit un nombre entre 1 et 50"
 
 user_number = gets.chomp.to_i
 
-if user_number > 0 && user_number <= 50
-    user_number.times do |i|
-        i += 1
-        puts i
-    end
-else
-    puts "Nop entre 1 et 50 stp"
+def Getinput(user_number)
+    condition(user_number)
 end
 
+def condition(b)
+    if b > 0 && b <= 50
+        b.times do |i|
+            i += 1
+            puts i
+        end
+    else
+        puts "Nop entre 1 et 50 stp"
+        puts "ecrit un nombre entre 1 et 50"
+        
+        Getinput(gets.chomp.to_i)
+        
+    end
+end
 
+Getinput(user_number)
