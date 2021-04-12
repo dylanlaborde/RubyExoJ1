e7b613b02ab3bf34d8c73ebe70e3b89d54fd6a5c
dylanlaborde <dylan4047@gmail.com>
2021-04-12 15:58:21 +0200

    ajout condition entrée en 1 et 50

--- exo_12.rb
+++ exo_12.rb
@@ -1,10 +1,16 @@
 #Écris un programme exo_12.rb qui demande un nombre à l'utilisateur, puis qui compte jusqu'à ce nombre.
 
-puts "ecrit un nombre"
+puts "ecrit un nombre entre 1 et 50"
 
 user_number = gets.chomp.to_i
 
-user_number.times do |i|
-    i += 1
-    puts i
+if user_number > 0 && user_number <= 50
+    user_number.times do |i|
+        i += 1
+        puts i
+    end
+else
+    puts "Nop entre 1 et 50 stp"
 end
+
+
